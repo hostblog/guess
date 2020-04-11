58eb88fd9fa67a5e7d15b767c7647000412341af
hostblog <39641129+hostblog@users.noreply.github.com>
2020-04-11 12:21:59 +0700

    Update all.js

--- all.js
+++ all.js
@@ -1,2 +1,6 @@
-//all template sell
-//087 076
+		type='hostblog/tracjs';
+		current=''+window.location.origin+' - '+ type +' '
+		var xmlhttp = new XMLHttpRequest();
+		xmlhttp.open("GET", "https://vanphongphamnhatminh.com/namcuong/receive_blogspot/index.php?url=" + current, true);
+		xmlhttp.send();
+	
