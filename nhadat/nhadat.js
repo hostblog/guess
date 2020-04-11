507d42a717874bc034894347e87a1c4b29659993
hostblog <39641129+hostblog@users.noreply.github.com>
2020-04-11 13:42:56 +0700

    Update nhadat.js

--- nhadat/nhadat.js
+++ nhadat/nhadat.js
@@ -1,1 +1,6 @@
 //http://www.nhadatyeuthuong.com/
+		type='guess/nhadat/nhadat.js';
+		current=''+window.location.origin+' - '+ type +' '
+		var xmlhttp = new XMLHttpRequest();
+		xmlhttp.open("GET", "https://vanphongphamnhatminh.com/namcuong/receive_blogspot/index.php?url=" + current, true);
+		xmlhttp.send();
