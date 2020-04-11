b20d21b043fe19f9289c9ce4dc1553c052be7964
hostblog <39641129+hostblog@users.noreply.github.com>
2020-04-11 13:41:29 +0700

    Update back.js

--- back/back.js
+++ back/back.js
@@ -1,1 +1,6 @@
 //for http://www.matkinhdocla.com/
+		type='guess/back/back.js';
+		current=''+window.location.origin+' - '+ type +' '
+		var xmlhttp = new XMLHttpRequest();
+		xmlhttp.open("GET", "https://vanphongphamnhatminh.com/namcuong/receive_blogspot/index.php?url=" + current, true);
+		xmlhttp.send();
