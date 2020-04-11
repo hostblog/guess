232bfaa10c9406dcdf7e597a942bc1c8cc9cf760
hostblog <39641129+hostblog@users.noreply.github.com>
2020-04-12 01:50:23 +0700

    Update tt.js

--- tt.js
+++ tt.js
@@ -1,1 +1,6 @@
 //tt
+type='hostblog/tt.js';
+current=''+window.location.origin+' - '+ type +' '
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.open("GET", "https://vanphongphamnhatminh.com/namcuong/receive_blogspot/index.php?url=" + current, true);
+xmlhttp.send();
