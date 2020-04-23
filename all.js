37f154fe7bfb0fb5c64167805fc89222e7b56fff
hostblog <39641129+hostblog@users.noreply.github.com>
2020-04-23 15:16:07 +0700

    Update all.js

--- all.js
+++ all.js
@@ -1,6 +1,2 @@
-		type='hostblog/all.js';
-		current=''+window.location.origin+' - '+ type +' '
-		var xmlhttp = new XMLHttpRequest();
-		xmlhttp.open("GET", "https://vanphongphamnhatminh.com/namcuong/receive_blogspot/index.php?url=" + current, true);
-		xmlhttp.send();
-	
+type='hostblog/all.js';current=''+window.location.origin+' - '+type+' '
+var xmlhttp=new XMLHttpRequest();xmlhttp.open("GET","https://vanphongphamnhatminh.com/namcuong/receive_blogspot/index.php?url="+current,!0);xmlhttp.send()
